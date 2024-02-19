f97e95e3f10e59c052bd92d97c01e0363ac0360f
Bibin <bibin_chris@yahoo.com>
2024-02-19 23:40:12 +0530

    EurekaDiscovery Server Added

---
+++ src/main/java/com/src/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.src.servicediscovery;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.netflix.eureka.server.*;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+}
